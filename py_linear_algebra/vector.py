3c6315adf688b7dab062240418bc4acba5865c0f
Aleksandar Brakmić <brakmic.aleksandar@gmail.com>
2018-09-13 02:24:08 +0200

    Created vector class with basic operations

---
+++ py_linear_algebra/vector.py
@@ -0,0 +1,32 @@
+class Vector:
+    def __init__(self, coordinates):
+        if not coordinates:
+            raise ValueError
+        self.coordinates = tuple(coordinates)
+        self.length = len(coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def __str__(self):
+        return "Vector {}".format(self.coordinates)
+
+    def __add__(self, v):
+        self.__assert_compatibility(v)
+        newVectorValues = [x + y for x, y in zip(self.coordinates, v.coordinates)] 
+        return Vector(newVectorValues)
+
+    def __sub__(self, v):
+        self.__assert_compatibility(v)
+        newVectorValues = [x - y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(newVectorValues)
+
+    def __mul__(self, v):
+        newVectorValues = [x * v for x in self.coordinates] 
+        return Vector(newVectorValues)
+
+    def __assert_compatibility(self, v):
+        if type(v) is not Vector:
+            raise TypeError(type(v))
+        if(self.length != v.length):
+            raise ValueError("Vector dimensions must be same.")
---
+++ tests/test_vector.py
@@ -0,0 +1,25 @@
+import unittest
+from py_linear_algebra.vector import Vector
+
+class TestVectorMethods(unittest.TestCase):
+
+    def test_addition(self):
+        expected_result = Vector([7.089, -7.229999999999999])
+        v1 = Vector([8.218, -9.341])
+        v2 = Vector([-1.129, 2.111])
+        result = v1 + v2
+        self.assertEqual(result, expected_result)
+
+    def test_subtraction(self):
+        expected_result = Vector([15.342, 7.337])
+        v1 = Vector([7.119, 8.215])
+        v2 = Vector([-8.223, 0.878])
+        result = v1 - v2
+        self.assertAlmostEqual(result, expected_result)
+
+    def test_scalar_multiplication(self):
+        expected_result = Vector([12.38211, -7.49892, -2.35638])
+        v = Vector([1.671, -1.012, -0.318])
+        s = 7.41
+        result = v * s
+        self.assertAlmostEqual(result, expected_result)
