83bb96c9ce975ee8e23a7391b08a42da8d71cec0
Aleksandar Brakmić <brakmic.aleksandar@gmail.com>
2018-09-18 21:59:30 +0200

    Vector class will now use Decimal instead of floating point numbers

--- py_linear_algebra/vector.py
+++ py_linear_algebra/vector.py
@@ -1,16 +1,17 @@
 from math import sqrt
 from math import acos
 from math import pi
+from decimal import Decimal, getcontext
 
 class Vector:
     def __init__(self, coordinates):
         if not coordinates:
             raise ValueError
-        self.coordinates = tuple(coordinates)
+        self.coordinates = tuple(Decimal(x) for x in coordinates)
         self.length = len(coordinates)
 
     def __eq__(self, v):
-        return self.coordinates == v.coordinates
+        self.coordinates == v.coordinates
 
     def __str__(self):
         return "Vector {}".format(self.coordinates)
@@ -26,7 +27,7 @@ class Vector:
         return Vector(new_coordinates)
 
     def __mul__(self, v):
-        new_coordinates = self.multiply_scalar(v)
+        new_coordinates = self.multiply_scalar(Decimal(v))
         return Vector(new_coordinates)
 
     def __assert_compatibility(self, v):
@@ -36,26 +37,71 @@ class Vector:
             raise ValueError("Vector dimensions must be same.")
 
     def multiply_scalar(self, s):
-        return  [x * s for x in self.coordinates] 
+        return [x * s for x in self.coordinates] 
 
     def dot_product(self, v):
         return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
 
     def angle_radians(self, v):
         try:
-            return acos(self.normalized().dot_product(v.normalized()))
+            return Decimal(acos(self.normalized().dot_product(v.normalized())))
         except ValueError:
             raise ValueError("Can't find angle if one of vectors is zero vector")
 
     def angle_degrees(self, v):
-        return self.angle_radians(v) * (180.0 / pi)
+        return self.angle_radians(v) * Decimal(180.0 / pi)
 
     def magnitude(self):
-        return sqrt(sum(x**2 for x in self.coordinates))
+        return Decimal(sqrt(sum(x**2 for x in self.coordinates)))
 
     def normalized(self):
         try:
             magnitude = self.magnitude()
-            return self * (1.0 / magnitude)
+            return self * (Decimal(1.0) / magnitude)
         except ZeroDivisionError:
             raise ValueError("Zero vectors can't be normalized")
+
+
+expected_result = Vector([7.089, -7.229999999999999])
+v1 = Vector([8.218, -9.341])
+v2 = Vector([-1.129, 2.111])
+result = v1 + v2
+print(result, expected_result)
+
+expected_result = Vector([15.342, 7.337])
+v1 = Vector([7.119, 8.215])
+v2 = Vector([-8.223, 0.878])
+result = v1 - v2
+print(result, expected_result)
+
+expected_result = Vector([12.38211, -7.49892, -2.35638])
+v = Vector([1.671, -1.012, -0.318])
+s = 7.41
+result = v * s
+print(result, expected_result)
+
+expected_result = 7.440282924728065
+result = Vector([-0.221, 7.437]).magnitude()
+print(result, expected_result)
+
+expected_result = Vector ([0.9339352140866403, -0.35744232526233])
+result = Vector([5.581, -2.136]).normalized()
+print(result, expected_result)
+
+expected_result = -41.382286
+v1 = Vector([7.887, 4.138])
+v2 = Vector([-8.802, 6.776])
+result = v1.dot_product(v2)
+print(result, expected_result)
+
+expected_result = 3.072026309837249
+v1 = Vector([3.183, -7.627])
+v2 = Vector([-2.668, 5.319])
+result = v1.angle_radians(v2)
+print(result, expected_result)
+
+expected_result = 60.27581120523091
+v1 = Vector([7.35, 0.221, 5.188])
+v2 = Vector([2.751, 8.259, 3.985])
+result = v1.angle_degrees(v2)
+print(result, expected_result)
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,56 +1,64 @@
 import unittest
 from py_linear_algebra.vector import Vector
+from decimal import Decimal, getcontext
 
 class TestVectorMethods(unittest.TestCase):
 
     def test_addition(self):
-        expected_result = Vector([7.089, -7.229999999999999])
+        getcontext().prec = 10
+        expected_result = Vector([7.088999999999999968025576891, -7.229999999999999094058011906])
         v1 = Vector([8.218, -9.341])
         v2 = Vector([-1.129, 2.111])
         result = v1 + v2
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
+        self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
 
     def test_subtraction(self):
-        expected_result = Vector([15.342, 7.337])
+        expected_result = Vector([15.3420000000000005258016244625, 7.33699999999999985522691758888])
         v1 = Vector([7.119, 8.215])
         v2 = Vector([-8.223, 0.878])
         result = v1 - v2
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
+        self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
+
 
     def test_scalar_multiplication(self):
-        expected_result = Vector([12.38211, -7.49892, -2.35638])
+        expected_result = Vector([12.3821100000000005402078784300, -7.49892000000000022279067479758, -2.35638000000000008138822948922])
         v = Vector([1.671, -1.012, -0.318])
         s = 7.41
         result = v * s
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
+        self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
+        self.assertAlmostEqual(result.coordinates[2], expected_result.coordinates[2])
 
     def test_magnitude(self):
-        expected_result = 7.440282924728065
+        expected_result = Decimal(7.440282924728065)
         result = Vector([-0.221, 7.437]).magnitude()
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result, expected_result)
         
     def test_normalization(self):
-        expected_result = Vector ([0.9339352140866403, -0.35744232526233])
+        expected_result = Vector ([0.933935214086640295130539147343, -0.357442325262329983594964055642])
         result = Vector([5.581, -2.136]).normalized()
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
+        self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
 
     def test_dot_product(self):
-        expected_result = -41.382286
+        expected_result = Decimal(-41.382286)
         v1 = Vector([7.887, 4.138])
         v2 = Vector([-8.802, 6.776])
         result = v1.dot_product(v2)
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result, expected_result)
 
     def test_angle_radians(self):
-        expected_result = 3.072026309837249
+        expected_result = Decimal(3.072026309837249)
         v1 = Vector([3.183, -7.627])
         v2 = Vector([-2.668, 5.319])
         result = v1.angle_radians(v2)
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result, expected_result)
 
     def test_angle_degrees(self):
-        expected_result = 60.27581120523091
+        expected_result = Decimal(60.27581120523091)
         v1 = Vector([7.35, 0.221, 5.188])
         v2 = Vector([2.751, 8.259, 3.985])
         result = v1.angle_degrees(v2)
-        self.assertEqual(result, expected_result)
+        self.assertAlmostEqual(result, expected_result)
