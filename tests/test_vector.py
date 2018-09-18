1abdfcfff3c354b5de3c53911131e19f75e6cee8
Aleksandar Brakmić <brakmic.aleksandar@gmail.com>
2018-09-18 22:05:55 +0200

    Made code a bit cleaner, deleted manual testing residue

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -6,7 +6,7 @@ class TestVectorMethods(unittest.TestCase):
 
     def test_addition(self):
         getcontext().prec = 10
-        expected_result = Vector([7.088999999999999968025576891, -7.229999999999999094058011906])
+        expected_result = Vector([7.089, -7.229999999999999])
         v1 = Vector([8.218, -9.341])
         v2 = Vector([-1.129, 2.111])
         result = v1 + v2
@@ -14,16 +14,15 @@ class TestVectorMethods(unittest.TestCase):
         self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
 
     def test_subtraction(self):
-        expected_result = Vector([15.3420000000000005258016244625, 7.33699999999999985522691758888])
+        expected_result = Vector([15.342, 7.337])
         v1 = Vector([7.119, 8.215])
         v2 = Vector([-8.223, 0.878])
         result = v1 - v2
         self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
         self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
 
-
     def test_scalar_multiplication(self):
-        expected_result = Vector([12.3821100000000005402078784300, -7.49892000000000022279067479758, -2.35638000000000008138822948922])
+        expected_result = Vector([12.38211, -7.49892, -2.35638])
         v = Vector([1.671, -1.012, -0.318])
         s = 7.41
         result = v * s
@@ -37,7 +36,7 @@ class TestVectorMethods(unittest.TestCase):
         self.assertAlmostEqual(result, expected_result)
         
     def test_normalization(self):
-        expected_result = Vector ([0.933935214086640295130539147343, -0.357442325262329983594964055642])
+        expected_result = Vector ([0.9339352140866403, -0.35744232526233])
         result = Vector([5.581, -2.136]).normalized()
         self.assertAlmostEqual(result.coordinates[0], expected_result.coordinates[0])
         self.assertAlmostEqual(result.coordinates[1], expected_result.coordinates[1])
